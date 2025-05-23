ceee7482b82938a3bd06f622f1fdbca61e300605
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-05-23 22:44:26 +0530

    Fix teacher model import path

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Teacher = require('../models/teacher');
+const Teacher = require('../models/Teacher');
 
 // Get all teachers
 router.get('/', async (req, res) => {
